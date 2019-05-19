60b164701e684c80f9582e9b6cf3f10b4a74778e
ChickenBaby <lunaticsign@naver.com>
2019-05-19 21:03:00 +0900

    Update and rename ChampagneTower_NotSolved.py to ChampagneTower.py

--- Algorithm/leetcode/ChampagneTower.py
+++ Algorithm/leetcode/ChampagneTower.py
@@ -20,10 +20,16 @@ Explanation: We poured 2 cups of champange to the top glass of the tower (which 
 
 
 풀이방법
-- 아직 못푼 문제... 분명 간단해보이는데 정작 구현하려니깐 어떤식으로 해야될지 모르겠네...
-- 일단 제일 단순하게 재귀로...
-- 단순히 한 잔마다 2개씩 나눠지는 형태로 계산함
-
+- 이전
+  - 아직 못푼 문제... 분명 간단해보이는데 정작 구현하려니깐 어떤식으로 해야될지 모르겠네...
+  - 일단 제일 단순하게 재귀로...
+  - 단순히 한 잔마다 2개씩 나눠지는 형태로 계산함
+- 수정
+  - 위 방법을 그대로 다이나믹으로 변경
+  - 단 기존에는 dfs였다면 이번에는 bfs로 한 단계씩 각 유리잔에 들어갈 값을 계산하는 형태로 함
+  - 위에서 잔에 붓고 남은걸 다시 아래 레이어에 반씩 넘겨주는 형태
+  - 풀리긴 했는데... 속도가 매우 느림... 타임리밋 아슬아슬하게 걸린 느낌?
+  - 추후 개선해봐야함
 
 """
 from collections import deque 
@@ -32,7 +38,7 @@ class Solution:
     def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
         tower = [[0.0 for _ in range(i+1)] for i in range(100)]
         
-        
+        """
         def fillTower(poured, row, glass):
             tower[row][glass] += poured
             
@@ -49,5 +55,25 @@ class Solution:
                 fillTower(remained/2.0, row+1, glass+1)
             
         fillTower(poured, 0, 0)
+        """
+        
+        eachPoured = [poured]
+        for i in range(100):
+            newEach = [0.0 for _ in range(i+2)]
+            cnt = 0
+            for j in range(i+1):
+                tower[i][j] += eachPoured[j]
+                
+                if tower[i][j]>1.0:
+                    remained = tower[i][j] - 1.0
+                    tower[i][j] = 1.0
+                    newEach[j] += remained/2.0
+                    newEach[j+1] += remained/2.0
+                    cnt += 1
+            
+            if cnt==0:
+                break
+                
+            eachPoured = newEach
         
         return tower[query_row][query_glass]
