dded25bb5ab35750acc2968c23cd1cd3896f42af
ChickenBaby <lunaticsign@naver.com>
2020-05-01 23:46:00 +0900

    Create DeepestLeavesSum.scala

---
+++ Algorithm/leetcode/DeepestLeavesSum.scala
@@ -0,0 +1,18 @@
+/*
+주소: https://leetcode.com/problems/deepest-leaves-sum/
+
+내용
+- 자연수로 이루어진 바이너리 트리가 주어진다
+- 트리의 가장 깊은 레벨 노드들의 합을 구하라
+
+샘플
+Input: root = [1,2,3,4,5,null,6,7,null,null,null,null,8]
+Output: 15
+
+
+풀이방법
+- 
+
+
+*/
+
