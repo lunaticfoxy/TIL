eaa33544040032099aa16e4990df668e5fdb3420
ChickenBaby <lunaticsign@naver.com>
2020-06-16 20:56:22 +0900

    Update SmallestSufficientTeam(NotSolved).scala

--- Algorithm/leetcode/SmallestSufficientTeam(NotSolved).scala
+++ Algorithm/leetcode/SmallestSufficientTeam(NotSolved).scala
@@ -18,10 +18,11 @@ Output: [1,2]
 풀이방법
 - 재귀로 시도
   - 먼저 필요한 스킬셋과 사람별 스킬셋을 set으로 만듬
-  - 이후 ㅎ
-  - 먼저 필요한 스킬셋과 사람별 스킬셋을 set으로 만ㅅ
-  - 먼저 필요한 스킬셋과 사람별 스킬셋을 set으로 만
-
+  - 이후 한명씩 비교하면서 넣었을때와 아닐때를 재귀로 나누어 탐색
+    - 이때 남은 스킬셋을 충족하는 경우만 넣음
+  - 스킬셋이 모두 충족되면 현재까지 인원 리턴
+  - 스킬셋이 충족되지 않았는데 남은 인원이 없으면 null 리턴
+  - 답은 나오나 타임리밋 발생
 """
 object Solution {
     def smallestSufficientTeam(req_skills: Array[String], people: List[List[String]]): Array[Int] = {
