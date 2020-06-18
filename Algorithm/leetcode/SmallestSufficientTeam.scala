33d35d615d2f897d701724fbc8f78987887cea27
ChickenBaby <lunaticsign@naver.com>
2020-06-19 00:36:52 +0900

    Update and rename SmallestSufficientTeam(NotSolved).scala to SmallestSufficientTeam.scala

--- Algorithm/leetcode/SmallestSufficientTeam.scala
+++ Algorithm/leetcode/SmallestSufficientTeam.scala
@@ -23,34 +23,34 @@ Output: [1,2]
   - 스킬셋이 모두 충족되면 현재까지 인원 리턴
   - 스킬셋이 충족되지 않았는데 남은 인원이 없으면 null 리턴
   - 답은 나오나 타임리밋 발생
+- 재귀 시도 2차
+  - 다음 시도는 스킬셋 단위로 스킬을 가지고 있는 사람의 인덱스를 저장하도록 구성
+  - 이후 스킬셋 단위로 재귀를 돌면서 해당 스킬을 가지고 있는 사람을 한명씩 선택하면서 다음 단계로 재귀 수행
+    - 이때 선택된 사람이 포함된 스킬은 지우고 탐색
 """
 object Solution {
     def smallestSufficientTeam(req_skills: Array[String], people: List[List[String]]): Array[Int] = {
         
-        
-        def recurFunc(reqSkill: Set[String], people: List[Set[String]], idx: Int, idxs: Array[Int]): Array[Int] = {
+        def recurFunc(reqSkill: Array[Set[Int]], people: Array[Int]): Array[Int] = {
             if(reqSkill.size == 0)
-                idxs
-            else if(people.size == 0)
+                people
+            else if(reqSkill(0).size == 0)
                 null
             else {
-                val cont = if(reqSkill.intersect(people(0)).size == 0) null else recurFunc(reqSkill.filter(x => !people(0).contains(x)), people.drop(1), idx + 1, idxs ++ Array(idx))
-                val notCont = recurFunc(reqSkill, people.drop(1), idx + 1, idxs)
+                val temp = reqSkill(0).map{person => 
+                    recurFunc(reqSkill.drop(1).filter(x => !x.contains(person)), people ++ Array(person))
+                }.filter(x => x != null)
                 
-                if(cont == null)
-                    notCont
-                else if(notCont == null)
-                    cont
-                else {
-                    if (cont.size > notCont.size)
-                        notCont
-                    else
-                        cont
-                }
+                temp.reduce((x, y) => if(x.size < y.size) x else y)
             }
         }
         
+        val peopleWithIdx = people.zipWithIndex
         
-        recurFunc(req_skills.toSet, people.map(_.toSet), 0, Array())
+        val reqSkill = req_skills.map{x =>
+            peopleWithIdx.filter(y => y._1.contains(x)).map(_._2).toSet
+        }.toArray
+        
+        recurFunc(reqSkill, Array[Int]())
     }
 }
