689702cb7a4660fe80ef43ecaa7ed5181e393082
ChickenBaby <lunaticsign@naver.com>
2021-02-17 21:42:42 +0900

    Update calcRatio.scala

--- Scala/example/calcRatio.scala
+++ Scala/example/calcRatio.scala
@@ -2,5 +2,5 @@ def calcRatio(arr: Array[(Int, Int)], lastSum: Int, allSum: Int, arrRes: Array[(
   if(arr == null || arr.isEmpty)
     arrRes
   else
-    calcRatio(arr.drop(1), lastSum + arr(0)._2, allSum, arrRes ++ Array((arr(0)._1, arr(0)._2, lastSum + arr(0)._2, (arrRes.last._3 + arr(0)._1).toDouble / allSum.toDouble)))
+    calcRatio(arr.drop(1), lastSum + arr(0)._2, allSum, arrRes ++ Array((arr(0)._1, arr(0)._2, lastSum + arr(0)._2, (lastSum + arr(0)._2).toDouble / allSum.toDouble)))
 }
